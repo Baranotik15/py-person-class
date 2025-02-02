9ca39ae30d15e195f3666bd160cb93caee21e682
RuslanTopal <baranotik15@gmail.com>
2025-02-02 20:18:47 +0000

    correct my mistake

--- app/main.py
+++ app/main.py
@@ -9,36 +9,33 @@ class Person:
         Person.people[name] = self
 
     def set_wife(self, wife: dict) -> None:
-        """Присваиваем жену"""
+        """add wife"""
         self.wife = wife
         wife.husband = self
 
     def set_husband(self, husband: dict) -> None:
-        """Присваиваем мужа"""
+        """add husband"""
         self.husband = husband
         husband.wife = self
 
 
 def create_person_list(person_data: list) -> list:
-    """Создает список из всех элементов словаря"""
-    person_list = []
-    for data in person_data:
-        person = Person(data["name"], data["age"])
-        person_list.append(person)
+    """Creates a list of all dictionary elements"""
+    persons = {
+        data["name"]: Person(data["name"], data["age"]) for data in person_data
+    }
 
-        # Присваиваем супругов
+    for data in person_data:
+        person = persons[data["name"]]
+
         if "wife" in data and data["wife"]:
-            wife = next(
-                (p for p in person_list if p.name == data["wife"]), None
-            )
+            wife = persons.get(data["wife"])
             if wife:
                 person.set_wife(wife)
 
         if "husband" in data and data["husband"]:
-            husband = next(
-                (p for p in person_list if p.name == data["husband"]), None
-            )
+            husband = persons.get(data["husband"])
             if husband:
                 person.set_husband(husband)
 
-    return person_list
+    return list(persons.values())
